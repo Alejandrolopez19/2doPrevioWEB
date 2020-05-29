6976130375be4e290190b2524545ab26b50d92dd
Alejandrolopez19 <alejandro190920@gmail.com>
2020-05-29 11:41:06 -0500

    Segundo Previo 1commit

---
+++ src/Controller/login.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Dto.Alumno;
+import Dto.Asignatura;
+import Dto.Categoria;
+import Dto.Categoria;
+
+/*
+import Negocio.Tiendas;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+*/
+/**
+ *
+ * @author Pc-Victor
+ */
+public class login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet login</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet login at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+
+        String email = request.getParameter("email");
+        String clave = request.getParameter("clave");
+        Cliente cli = new Cliente();
+        //System.out.println("clave"+clave+"Email"+email);
+        cli.setEmail(email);
+        cli.setClave(clave);
+
+        Tienda tie = new Tienda();
+        tie.setEmail(email);
+        tie.setClave(clave);
+
+        Tiendas t = new Tiendas();
+     
+        
+        if (t.login(cli)) {
+            //haciendo uso de jsp
+            request.getRequestDispatcher("servicios.html").forward(request, response);
+        } else if (t.loginTienda(tie)) {
+            // request.getRequestDispatcher("error.jsp").forward(request, response);
+
+            request.getRequestDispatcher("tienda.html").forward(request, response);
+
+        } else {
+            request.getRequestDispatcher("error.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
